007a190f7c1cdf176976805e15289a5e15cf63f9
Eric Swanson <es20641@gmail.com>
2024-12-24 11:23:44 -0600

    init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	"github.com/jroimartin/gocui"
+)
+
+func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("main", 0, 0, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "LazyRest"
+		v.Wrap = true
+		v.Write([]byte("Welcome to LazyRest!"))
+	}
+	return nil
+}
